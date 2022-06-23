216281cf261f5b4ef4199376d8d9e37cf2aad6b3
Admon <bond.007.911@mail.ru>
2022-06-23 18:04:06 +0300

    Added 14 task with presentation

--- Task.3/hw3.sh
+++ Task.3/hw3.sh
@@ -8,10 +8,25 @@ api_token='KbpJp9xNI9souGm8tehQmOMSERPrqHLLSX9acXxR'
 
 
 
-record_ip4=$(
-  $curl -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=A" \
-    -H "X-Auth-Email: $auth_email" \
-    -H "X-Auth-Key: $auth_key" \
-    -H "Content-Type: application/json" \
-    | $jq '.result[0].content' --raw-output
-)
+#record_ip4=$(
+#  $curl -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=A" \
+#    -H "X-Auth-Email: $auth_email" \
+#    -H "X-Auth-Key: $auth_key" \
+#    -H "Content-Type: application/json" \
+#    | $jq '.result[0].content' --raw-output
+#)
+
+
+#ip=$(curl -s -X GET https://checkip.amazonaws.com)
+#echo "Current IP is $ip"
+#if host $dnsrecord 1.1.1.1 | grep "has address" | grep "$ip"; then
+#  echo "$dnsrecord is currently set to $ip; no changes needed"
+#  exit
+#fi
+
+
+`curl -X POST "https://api.cloudflare.com/client/v4/zones/1e3c80ff28de72305180a86398f3d264/dns_records" \
+ -H "Authorization: Bearer KbpJp9xNI9souGm8tehQmOMSERPrqHLLSX9acXxR"\ 
+ -H "Content-Type: application/json" \ 
+ --data 
+ '{"type":"A","name":"admon2.com","content":"127.0.0.1","ttl":120,"priority":10,"proxied":false}'`
