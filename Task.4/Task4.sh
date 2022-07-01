02075a4aa22f239972ce916ef26842bd8c7ae77b
Admon <bond.007.911@mail.ru>
2022-07-01 13:59:47 +0300

    ip verification fixes have been made

--- Task.4/Task4.sh
+++ Task.4/Task4.sh
@@ -1,57 +1,46 @@
 #!/bin/bash
-export PATH=/sbin:/opt/bin:/usr/local/bin:/usr/contrib/bin:/bin:/usr/bin:/usr/sbin:/usr/bin/X11
+source /home/Task4.conf
+	
+sudo sh -c "echo 'nameserver=adelaide.ns.cloudflare.com' >> /etc/resolv.conf"
 
-
-ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+ip=$(ifconfig enp0s3 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 echo "Current IP is $ip"
 
-if which nslookup >/dev/null; then
-DNS_ip=$(nslookup ${DNS} 1.1.1.1 | awk '/Address/ { print $2 }' | sed -n '2p')
-is_proxed="${PROXY}"
-fi
-
-#curl -s -X GET "${URL}/${DNS_ZONE}/DNS?type=A" -H "Authorization: Bearer ${API_TOKEN}" \
-#-H "Content-Type: application/json"|jq '.result[]|{id,name,type,content,ttl}'
-
 #Create DNS record
-#recordA_ip=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/dns_records" \
-#     -H "X-Auth-Email: $EMAIL_ADDRESS" \
-#     -H "X-Auth-Key: $AUTH_KEY" \
-#     -H "Content-Type: application/json" \
-#     --data '{"type":"A","name":"new32","content":"127.0.0.53","ttl":120,"priority":10,"proxied":false}' )
-#  if [[ $recordA_ip == *"\"success\":false"* ]]; then
-#        message="DNS Create FAILED."
-#        echo -e "$message"
-#        exit 1 
-#    else
-#        message="DNS CREATE SUCCESS "    
-#    fi
+recordA_ip=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/dns_records" \
+     -H "X-Auth-Email: $EMAIL_ADDRESS" \
+     -H "X-Auth-Key: $AUTH_KEY" \
+     -H "Content-Type: application/json" \
+     --data "{\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":\"120\",\"proxied\":false}")
+  if [[ $recordA_ip == *"\"success\":false"* ]]; then
+        message="DNS Create FAILED."
+        echo -e "$message"
+        exit 1 
+    else
+        message="DNS CREATE SUCCESS "    
+    fi
 
-
-echo " DNS record of ${DNS} is: ${ip}. Trying to update..."
-
-cloudflare_record_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$Zone_ID/dns_records?name=$DNS" \
--H "Authorization: Bearer $API_TOKEN" \
--H "Content-Type: application/json")
+echo " DNS record of ${record_name} is: ${ip}. Trying to update..."
+### Get the DNS record information from cloudflare's api
+cloudflare_record_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/dns_records?name=$record_name" \
+    -H "Authorization: Bearer $API_Token" \
+    -H "Content-Type: application/json")
 if [[ ${cloudflare_record_info} == *"\"success\":false"* ]]; then
-echo ${cloudflare_record_info}
-echo "Error! Can't get ${DNS} record inforamiton from cloudflare API"
-exit 0
+    echo ${cloudflare_record_info}
+    echo "Error! Can't get ${record_name} record inforamiton from cloudflare API"
+    exit 0
 fi
 
-### Get the dns record id from response
-cloudflare_DNS_id=$(echo ${cloudflare_record_info} | grep -o '"id":"[^"]*' | cut -d'"' -f4)
-
-### Push new dns record information to cloudflare's api
-update_DNS=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$Zone_ID/DNS/$cloudflare_DNS_id" \
--H "Authorization: Bearer $API_TOKEN" \
--H "Content-Type: application/json" \
---data "{\"type\":\"A\",\"name\":\"$DNS\",\"content\":\"$ip\",\"ttl\":$ttl,\"PROXY\":$PROXY}")
+### Push new DNS record information to cloudflare's api
+update_DNS=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/DNS/$cloudflare_record_info" \
+    -H "Authorization: Bearer $API_Token" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":120,\"PROXY\":false}")
 if [[ ${update_DNS} == *"\"success\":false"* ]]; then
-echo ${update_DNS}
-echo "Error! Update Failed"
+    echo ${update_DNS}
+    echo "Error! Update Failed"
 exit 0
 fi
 
 echo "= Success!"
-echo "= $DNS DNS Record Updated To: $ip, ttl: $ttl, PROXY: $PROXY"
+echo "= $record_name DNS Record Updated To: $ip, ttl: 120, PROXY: false"
