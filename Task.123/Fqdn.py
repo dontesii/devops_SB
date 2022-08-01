470c4b818d94a12c39e5be4df03537604ca9b8d2
Admon <bond.007.911@mail.ru>
2022-08-01 17:33:34 +0300

    fix code

--- Task.123/Fqdn.py
+++ Task.123/Fqdn.py
@@ -4,26 +4,31 @@ import email
 import os
 import requests
 import socket
-import file1
-
-from file1 import * 
+def main():
 
 
 
+#fqdn = input("enter fqdn- ")
 
-fqdn = input("enter fqdn- ")
+if len (sys.argv) > 3:
+            print ("Ошибка. Слишком много параметров.")
+            sys.exit (1)
+      elif len(sys.argv) == 2:
+        fqd_name = sys.argv[1]
+    else:
+        fqd_name = conf.vm_name       
 
+
+fqdn = vm_name + "." + conf.domain
 
 #Ip adress
-
 IP = print(socket.gethostbyname(socket.gethostname()))
 print(IP)
 
 #Create DNS record
-
 headers = {
     'X-Auth-Email': email,
-    'X-Auth-Key': auto_key,
+    'X-Auth-Key': conf.auto_key,
 }
 json_data = {
     'type': 'A',
@@ -33,7 +38,8 @@ json_data = {
     'priority': 10,
     'proxied': False,
 }
-response = requests.post('https://api.cloudflare.com/client/v4/zones/1e3c80ff28de72305180a86398f3d264/dns_records', headers=headers, json=json_data)
+ response = requests.get(f"https://api.cloudflare.com/client/v4/zones/{conf.dns_zone}/dns_records?&name={domain_name}", \
+        headers=my_headers).json()
 
 #### Get the DNS record information from cloudflare's api
 api_token = os.getenv('api_token')
@@ -42,8 +48,8 @@ headers = {
     'Authorization': f"Bearer {api_token}",
     'Content-Type': 'application/json',
 }
-
-response = requests.get('https://api.cloudflare.com/client/v4/zones/1e3c80ff28de72305180a86398f3d264/dns_records?name=fqdn', headers=headers)
+response = requests.get(f"https://api.cloudflare.com/client/v4/zones/{conf.dns_zone}/dns_records?&name={domain_name}", \
+        headers=my_headers).json()
 
 
 ### Push new DNS record information to cloudflare's api
@@ -59,4 +65,7 @@ json_data = {
     'PROXY': False,
 }
 
-response = requests.put('https://api.cloudflare.com/client/v4/zones/1e3c80ff28de72305180a86398f3d264/DNS/$cloudflare_record_info', headers=headers, json=json_data)
+requests.put(f"https://api.cloudflare.com/client/v4/zones/{cond.dns_zone}/dns_records/{dns_id}", \
+                headers=my_headers, json=json_data)
+
+main()
